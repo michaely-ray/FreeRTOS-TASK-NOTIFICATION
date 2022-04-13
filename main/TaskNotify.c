d03267faa647d0f1a6b30a363686250760743590
Wesley Souza <wesley.souza@PEINSMTNB2003.insole.local>
2022-04-13 13:08:17 -0300

    Using TaskNotify to make a semaphore

--- main/TaskNotify.c
+++ main/TaskNotify.c
@@ -1,54 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/event_groups.h"
 
-uint count = 0;
+static TaskHandle_t receiverHandler = NULL;
 
-TaskHandle_t handler1;
-TaskHandle_t handler2;
-
-void task1(void *params)
+void handleWifi(void *params)
 {
-    
     while (true)
-    { 
-        // printf("COUNT1: %d \n\n", count);
-
-        // if (ulTaskNotifyTake( pdFALSE, portMAX_DELAY))
-        if (xTaskNotifyWait(0xffffffff, 0, NULL, portMAX_DELAY))
-        {
-            printf("COUNT1: %d \n\n", count);
-            count++;
-            vTaskDelay(1000/ portTICK_PERIOD_MS);
-            xTaskNotify(handler2, 0, eSetValueWithOverwrite);
-        }
+    {
+        vTaskDelay(5000/ portTICK_PERIOD_MS);
+        printf("GET IP \n\n");
+        vTaskDelay(5000/ portTICK_PERIOD_MS);
+        xTaskNotifyGive(receiverHandler);
     }
 }
 
-void task2(void *params)
+void initMqtt(void *params)
 {
     while (true)
-    { 
-        // printf("COUNT2: %d \n\n", count);
-
-        if (xTaskNotifyWait( 0xffffffff, 0, NULL, portMAX_DELAY))
-        {
-        printf("COUNT2: %d \n\n", count);
-        count++;
-        vTaskDelay(1000/ portTICK_PERIOD_MS);
-        xTaskNotify(handler1, 0, eSetValueWithOverwrite);
-        }
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        printf("MQTT Start\n\n");
     }
 }
 
+void initWifi(void)
+{
+     xTaskCreate(&handleWifi, "handleWIFI", 2048, NULL, configMAX_PRIORITIES-1, NULL);
+}
+
+void connections(void)
+{
+    initWifi();
+    xTaskCreate(&initMqtt, "MQTT Start", 2048, NULL, configMAX_PRIORITIES-1, &receiverHandler);
+}
 
 void app_main(void)
 {
-  xTaskCreate(&task1, "COUNTER 1", 2048, NULL, configMAX_PRIORITIES-1, &handler1);
-  xTaskCreate(&task2, "COUNTER 2", 2048, NULL, configMAX_PRIORITIES-1, &handler2);
-  xTaskNotify(handler1, count, eSetBits);
-
+    connections();
 }
